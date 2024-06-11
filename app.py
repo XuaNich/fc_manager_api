9b7a380d8c3ba603fa28da3b78f74c2584a55831
xster1mil <xuanmkm@icloud.com>
2024-06-10 23:02:22 -0400

    Created a Python Package: Models w/ club, team, palyer and user python files

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 import os
 
+from SQLAlchemy_DB_ORM_Template.App import app
 from flask import Flask
+from dotenv import load_dotenv
 
 
-app = Flask(__name__)
+from db import db
 
 
-app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+def create_app():
+    load_dotenv()
 
+
+    app = Flask(__name__)
+
+
+    app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    return app
+
+
+app = create_app()
---
+++ db.py
@@ -0,0 +1,4 @@
+from flask import SQLAlchemy
+
+
+db = SQLAlchemy()
