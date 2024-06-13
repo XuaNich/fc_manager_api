5c02a3b7702d555a328d15aa07a456043e952aae
xster1mil <xuanmkm@icloud.com>
2024-06-13 17:37:19 -0400

    Added Swagger_UI

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ def create_app():
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPEN_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 
     app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
